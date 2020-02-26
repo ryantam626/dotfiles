24319722d741345784d6d80cb44a8989d15fc01b
Ryan Tam <ryantam626@gmail.com>
2020-02-26 23:07:20 +0000

    scripts: Add a simple bootstrap script
    This is kind of useless maybe, but would help if one wants to just copy
    and paste commands and have the whole setup running in the future.

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,25 @@
+set -e
+
+info() {
+	printf "  [ \033[00;34m..\033[0m ] $1"
+}
+
+success() {
+	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail() {
+	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+	echo ''
+	exit
+}
+
+bootstrap_install() {
+	sudo apt update
+	sudo apt install curl git
+}
+
+info "Yo dawg I heard you like installations, so I am installing some stuff so you can install even more stuff.\n"
+
+bootstrap_install || fail "Bootstrap failed - NANI!?\n"
+success "All done, goodbye.\n"
