18d050d2ebb5bb8c2a94aa3e1210ebb0f865025d
Ryan Tam <ryantam626@gmail.com>
2020-09-17 18:33:48 +0100

    add some default packages that python compilation will probably need
    ref: https://github.com/asdf-vm/asdf/issues/570#issuecomment-531187568

--- installers/installers.sh
+++ installers/installers.sh
@@ -196,6 +196,7 @@ install_docker() {
 	sudo chmod +x /usr/local/bin/docker-compose
 }
 
-install_pyenv() { 
-	git clone https://github.com/pyenv/pyenv.git ~/.pyenv    
+install_pyenv() {
+	sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 }
