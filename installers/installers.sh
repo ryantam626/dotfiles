e9fc095fd5425f5936824f1afa287d439fe87898
Ryan Tam <ryantam626@gmail.com>
2020-10-29 15:36:03 +0000

    Add `-y` for apt install in pyenv installation

--- installers/installers.sh
+++ installers/installers.sh
@@ -197,6 +197,6 @@ install_docker() {
 }
 
 install_pyenv() {
-	sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+	sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 }
